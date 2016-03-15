9b45c83d8306b24fe4eeed845e255956f4f04b6d
woodstkp <woodstkp@mcmaster.ca>
2016-03-14 21:04:38 -0400

    nsp regression

---
+++ number_sex_partners_regression.R
@@ -0,0 +1,21 @@
+setwd("~/Dropbox/Woodstock_thesis")
+
+library(lme4)
+load("merge_HIV.RData")
+
+mAnswers <- subset(Answers, gender=="Male")
+fAnswers <- subset(Answers, gender=="Female")
+
+
+HIV_nsp <- glmer(formula = hiv_pos ~ log(nb.sex.partner)+ Age 
+  + marital_status + (1 | Country.code.and.phase/Cluster.number), 
+  family = binomial(logit), data = fAnswers
+)
+
+HIV_nsp2 <- glmer(formula = hiv_pos ~ log(nb.sex.partner)+ Age 
+  + marital_status + (1 | Country.code.and.phase), 
+  family = binomial(logit), data = fAnswers
+)
+
+anova(HIV_nsp,HIV_nsp2)
+summary(HIV_nsp)
