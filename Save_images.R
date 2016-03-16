5ad810cb7aa2969df4652966949fb652cb8dff1b
woodstkp <woodstkp@mcmaster.ca>
2016-03-15 20:17:37 -0400

    more plots added

--- Save_images.R
+++ Save_images.R
@@ -1,7 +1,9 @@
 library(ggplot2)
 library(splines)
 setwd("~/Dropbox/Woodstock_thesis")
-load("merged_data_spline_plots.R")
+
+
+load("merged_data_spline_plots.RData")
 
 ggsave(file="HIV_risk_by_gender.png",plot=HIV_risk_by_gender)
 ggsave("HIV_proportion_by_country.png",plot=HIV_proportion_by_country)
@@ -13,3 +15,7 @@ ggsave("HIV_prob_current_marital_status_and_gender.png",
 ggsave("HIV_prob_current_marital_status_and_country.png",
        plot=HIV_prob_current_marital_status_and_country)
 ggsave("HIV_prob_marital_status_and_country.png",plot=HIV_prob_marital_status_and_country)
+
+load("Risk_factor_spline_plots.RData")
+ggsave("HIV_and_age_of_sexual_debut.png",plot=HIV_and_age_of_sexual_debut)
+ggsave("HIV_and_number_sex_partners.png",plot=HIV_and_number_sex_partners)
