da849eef0f82cccab6185de827a8528264541f79
woodstkp <woodstkp@mcmaster.ca>
2016-04-14 21:25:30 -0400

    recent marriage plot created

---
+++ recent_marriage_analysis.R
@@ -0,0 +1,28 @@
+setwd("~/Dropbox/Woodstock_thesis")
+
+library("ggplot2")
+library("splines")
+
+load("merge_couple_HIV.RData")
+load("merge_HIV.RData")
+
+recentmarr <- subset(Answers.couple, recent_marriage == TRUE)
+nevermarr <- subset(Answers, marital_status == "never" & gender == "Female")
+
+## Plotting graph
+print(
+  ggplot()
+    +geom_smooth(data=nevermarr, aes(
+      x=Age, y= as.numeric(hiv_pos),col="Never Married"), 
+      method="glm",method.args=list("binomial"), formula=y~ns(x, 4),na.rm=FALSE)
+  
+    +geom_smooth(
+      data=recentmarr, aes(
+        x=current.age.f, y= as.numeric(hiv_pos),col="Recently Married"),
+        method="glm",method.args=list("binomial"), formula=y~ns(x, 4),na.rm=FALSE)
+  
+  + ylab("Proportion HIV+")
+  + ggtitle("HIV Prevalence in Never Versus Recently Married Individuals")
+  + scale_color_manual("Marital Status", values = c("red","blue") )
+)
+
